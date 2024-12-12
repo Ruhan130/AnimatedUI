7329ebd27d7c1355fa507b29371c241d32bd999e
Ruhan130 <ruhank170@gmail.com>
2024-12-12 12:39:26 +0500

    Start working on Create NEw Password Screen

--- lib/Presentation/CreateNewPassord/CreateNewPasswordScreen.dart
+++ lib/Presentation/CreateNewPassord/CreateNewPasswordScreen.dart
@@ -5,6 +5,8 @@ class CreateNewPasswordScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
